6f03283d802fd0089ba5ef3c661c0ab087004aa9
Augustin Fabbro <fabbroaugustin@gmail.com>
2025-07-10 19:50:30 +0000

    début vidéo

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 import os
 import subprocess
 
-nb = 163
+nb = 200
+sf = 13  # Nombre de vidéos déjà créées
 
-i = 36
-
-for i in range(nb):
+for i in range(sf, nb):
     subprocess.run(['python3', 'Bot.py'], check=True)
     os.rename('output_with_audio.mp4', f'vidéo_n° {i + 1}.mp4')
---
+++ test.py
@@ -0,0 +1,2 @@
+for i in range(13, 200):
+    print(i)
