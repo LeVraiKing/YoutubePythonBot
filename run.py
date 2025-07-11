4309899f2c5fef9a6b6051aa0be3dd659b3e4d89
LeVraiKing <fabbroaugustin@gmail.com>
2025-07-11 12:32:35 +0000

    modif

--- run.py
+++ run.py
@@ -8,4 +8,4 @@ sf = 103  # Nombre de vidéos déjà créées
 for i in range(sf, nb):
     subprocess.run(['python3', 'Bot.py'], check=True)
     os.rename('output_with_audio.mp4', f'vidéo_n° {i + 1}.mp4')
-    shutil.move(f"vidéo_n°{i + 1}.mp4", "vidéos/")
+    shutil.move(f"vidéo_n° {i + 1}.mp4", "vidéos/")
