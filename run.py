afb66a34ee059d6bde3e944cee9ae5a9032c8959
LeVraiKing <fabbroaugustin@gmail.com>
2025-07-11 12:31:51 +0000

    déplacer

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 import os
 import subprocess
+import shutil
 
-nb = 200
-sf = 83  # Nombre de vidéos déjà créées
+nb = 200 # Nombre de vidéos à créer
+sf = 103  # Nombre de vidéos déjà créées
 
 for i in range(sf, nb):
     subprocess.run(['python3', 'Bot.py'], check=True)
     os.rename('output_with_audio.mp4', f'vidéo_n° {i + 1}.mp4')
+    shutil.move(f"vidéo_n°{i + 1}.mp4", "vidéos/")
